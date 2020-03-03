bc93bf1b75697108e673da8742e50fcb9dd8c55d
gizlu <gizlu@mailbox.org>
2020-03-03 16:54:52 +0100

    Improve readilibity
    Rename few variables

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 def get_stop_list():
     bus_stop = namedtuple(
         'bus_stop',
-        ['stop_id', 'time_table_id', 'stop_name', 'latitude', 'longitude'])
+        ['mpk_id', 'rozklady_lodz_id', 'stop_name', 'latitude', 'longitude'])
 
     indexes = [2, 0, 1, 5, 4]
 
     url = 'http://rozklady.lodz.pl/Home/GetMapBusStopList'
     resp = requests.post(url).json()
 
-    ret = {}
+    stop_list = {}
     for entry in resp:
         record = bus_stop(*[entry[i] for i in indexes])
-        ret[record.stop_id] = record
-    return ret
+        stop_list[record.mpk_id] = record
+    return stop_list
 
 
 STOP_LIST = get_stop_list()
@@ -29,15 +29,15 @@ STOP_LIST = get_stop_list()
 @app.route('/')
 def main_view():
     return '''
-    <form action="/redirect"><input name="stop_id">
+    <form action="/redirect"><input name="mpk_id">
     '''
 
 
 @app.route('/redirect')
 def redirect_view():
-    stop_id = str(int(request.args['stop_id']))
-    new_id = str(STOP_LIST[stop_id].time_table_id)
-    url = 'http://rozklady.lodz.pl/Home/TimeTableReal?busStopId=' + new_id
+    mpk_id = str(int(request.args['mpk_id']))
+    rozklady_lodz_id = str(STOP_LIST[mpk_id].rozklady_lodz_id)
+    url = 'http://rozklady.lodz.pl/Home/TimeTableReal?busStopId=' + rozklady_lodz_id
     return redirect(location=url)
 
 if __name__ == '__main__':
